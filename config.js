e318772e4f1ec33457dca387cb1ad3b37c14bc4a
tobi <tobiemba@gmail.com>
2025-02-05 15:23:17 +0100

    bien

--- config.js
+++ config.js
@@ -1,10 +1,22 @@
-const { Pool } = require('pg'); //importation du pool de connection qui sera utilisé dans des requettes
+// const { Pool } = require('pg'); //importation du pool de connection qui sera utilisé dans des requettes
 
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'postgres',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'HOSPITALS'
+//   });
+//   module.exports = pool; // pour exporter les configuration de la 
+
+
+const { Pool } = require('pg'); // Importation du pool de connexion
+require('dotenv').config(); // Charger les variables d'environnement depuis le fichier .env
+
+// Créer une nouvelle instance de Pool avec l'URL de connexion PostgreSQL
 const pool = new Pool({
-    user: 'postgres',
-    password: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'HOSPITALS'
-  });
-  module.exports = pool; // pour exporter les configuration de la 
+  connectionString: process.env.POSTGRES_URL, // Utilisation de la variable d'environnement POSTGRES_URL
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false // SSL pour Vercel, si en production
+});
+
+module.exports = pool;
